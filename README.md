0c1f3cabe756bc9d5e0d80e706a1bccb38afcb2a
psampaz <psampaz@gmail.com>
2019-11-26 16:28:36 +0200

    update readme and comments

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Go Report Card](https://goreportcard.com/badge/github.com/psampaz/slice)](https://goreportcard.com/report/github.com/psampaz/slice)
 [![golangci](https://golangci.com/badges/github.com/psampaz/slice.svg)](https://golangci.com/r/github.com/psampaz/slice)
 
-Typesafe functions for common Go slice operations.
+Type-safe functions for common Go slice operations.
 
 
 # Operations 
@@ -30,3 +30,31 @@ Typesafe functions for common Go slice operations.
 | uint32     | ✔   |
 | uint64     | ✔   |
 | uintptr    | ✔   |
+
+# Examples
+
+## Min
+
+Min returns the minimum value of a slice or an error in case of a nil or empty slice.
+```go
+    a := []int{1, 2, 3, 0, 4, 5}
+    min, err := slice.MinInt(a)
+```
+
+# Contributing
+
+You are very welcome to contribute new opearations in this library.
+
+The minimum requirements to accept a pull are the following:
+
+- 100% code coverage
+- Pass golangci checks
+- Precise documentation of each function
+- If the PR changes an existing operation
+    - apply changes to all functions of this operation
+- If the PR introduces a new operation, then:
+    - implement functions for all applicable types
+    - include one testable example
+    - update the table on the Operations section
+    - add one example int he Examples section
+
--- min.go
+++ min.go
@@ -3,7 +3,7 @@ package slice
 
 import "errors"
 
-// MinByte returns the minimum value of a byte slice
+// MinByte returns the minimum value of a byte slice or an error in case of a nil or empty slice
 func MinByte(a []byte) (byte, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -19,7 +19,7 @@ func MinByte(a []byte) (byte, error) {
 	return min, nil
 }
 
-// MinFloat32 returns the minimum value of a float32 slice
+// MinFloat32 returns the minimum value of a float32 slice or an error in case of a nil or empty slice
 func MinFloat32(a []float32) (float32, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -35,7 +35,7 @@ func MinFloat32(a []float32) (float32, error) {
 	return min, nil
 }
 
-// MinFloat64 returns the minimum value of a float64 slice
+// MinFloat64 returns the minimum value of a float64 slice or an error in case of a nil or empty slice
 func MinFloat64(a []float64) (float64, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -51,7 +51,7 @@ func MinFloat64(a []float64) (float64, error) {
 	return min, nil
 }
 
-// MinInt returns the minimum value of an int slice
+// MinInt returns the minimum value of an int slice or an error in case of a nil or empty slice
 func MinInt(a []int) (int, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -67,7 +67,7 @@ func MinInt(a []int) (int, error) {
 	return min, nil
 }
 
-// MinInt8 returns the minimum value of an int8 slice
+// MinInt8 returns the minimum value of an int8 slice or an error in case of a nil or empty slice
 func MinInt8(a []int8) (int8, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -83,7 +83,7 @@ func MinInt8(a []int8) (int8, error) {
 	return min, nil
 }
 
-// MinInt16 returns the minimum value of an int16 slice
+// MinInt16 returns the minimum value of an int16 slice or an error in case of a nil or empty slice
 func MinInt16(a []int16) (int16, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -99,7 +99,7 @@ func MinInt16(a []int16) (int16, error) {
 	return min, nil
 }
 
-// MinInt32 returns the minimum value of an int32 slice
+// MinInt32 returns the minimum value of an int32 slice or an error in case of a nil or empty slice
 func MinInt32(a []int32) (int32, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -115,7 +115,7 @@ func MinInt32(a []int32) (int32, error) {
 	return min, nil
 }
 
-// MinInt64 returns the minimum value of an int64 slice
+// MinInt64 returns the minimum value of an int64 slice or an error in case of a nil or empty slice
 func MinInt64(a []int64) (int64, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -131,7 +131,7 @@ func MinInt64(a []int64) (int64, error) {
 	return min, nil
 }
 
-// MinRune returns the minimum value of a rune slice
+// MinRune returns the minimum value of a rune slice or an error in case of a nil or empty slice
 func MinRune(a []rune) (rune, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -147,7 +147,7 @@ func MinRune(a []rune) (rune, error) {
 	return min, nil
 }
 
-// MinUint returns the minimum value of a uint slice
+// MinUint returns the minimum value of a uint slice or an error in case of a nil or empty slice
 func MinUint(a []uint) (uint, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -163,7 +163,7 @@ func MinUint(a []uint) (uint, error) {
 	return min, nil
 }
 
-// MinUint8 returns the minimum value of a uint8 slice
+// MinUint8 returns the minimum value of a uint8 slice or an error in case of a nil or empty slice
 func MinUint8(a []uint8) (uint8, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -179,7 +179,7 @@ func MinUint8(a []uint8) (uint8, error) {
 	return min, nil
 }
 
-// MinUint16 returns the minimum value of a uint16 slice
+// MinUint16 returns the minimum value of a uint16 slice or an error in case of a nil or empty slice
 func MinUint16(a []uint16) (uint16, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -195,7 +195,7 @@ func MinUint16(a []uint16) (uint16, error) {
 	return min, nil
 }
 
-// MinUint32 returns the minimum value of a uint32 slice
+// MinUint32 returns the minimum value of a uint32 slice or an error in case of a nil or empty slice
 func MinUint32(a []uint32) (uint32, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -211,7 +211,7 @@ func MinUint32(a []uint32) (uint32, error) {
 	return min, nil
 }
 
-// MinUint64 returns the minimum value of a uint64 slice
+// MinUint64 returns the minimum value of a uint64 slice or an error in case of a nil or empty slice
 func MinUint64(a []uint64) (uint64, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
@@ -227,7 +227,7 @@ func MinUint64(a []uint64) (uint64, error) {
 	return min, nil
 }
 
-// MinUintptr returns the minimum value of a uintptr slice
+// MinUintptr returns the minimum value of a uintptr slice or an error in case of a nil or empty slice
 func MinUintptr(a []uintptr) (uintptr, error) {
 	if len(a) == 0 {
 		return 0, errors.New("Cannot get the minimum of a nil or empty slice")
