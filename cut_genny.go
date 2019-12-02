2e92775456a0cfb1953d3597483b616004f8a6c5
psampaz <psampaz@gmail.com>
2019-12-02 19:49:09 +0200

    added first draft of genny cut

---
+++ cut_genny.go
@@ -0,0 +1,29 @@
+package slice
+
+import (
+	"errors"
+	"github.com/cheekybits/genny/generic"
+)
+
+type Type generic.Type
+
+// CutType deletes all elements between from and to index (inclusive), from a Type slice
+func CutType(a []Type, from, to Type) ([]Type, error) {
+	if len(a) == 0 {
+		return nil, errors.New("Can't cut a nil or empty slice")
+	}
+
+	if from < 0 {
+		return nil, errors.New("from index out of bounds")
+	}
+
+	if to > len(a)-1 {
+		return nil, errors.New("to index out of bounds")
+	}
+
+	if from > to {
+		return nil, errors.New("from index must be less or equal to to index")
+	}
+
+	return append(a[:from], a[to+1:]...), nil
+}
